604d374ec567250922bd9ecdee056e04ae88ee02
qfdy-l <312638221@qq.com>
2016-11-29 11:22:38 +0800

    将config-server注册为eureka服务

--- config-server/sboot-config-server/src/main/java/com/looqy/Application.java
+++ config-server/sboot-config-server/src/main/java/com/looqy/Application.java
@@ -2,10 +2,12 @@ package com.looqy;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(Application.class).web(true).run(args);
